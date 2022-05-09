c53ff7739562b58d4ec2706a84bb783b01bd4a31
almaskilichov-prog <itsmywaytoamerica@gmail.com>
2022-05-09 14:58:02 +0600

    1

---
+++ app/src/main/java/com/example/burgermenu/ui/rectsquare/RectSquareFragment.java
@@ -0,0 +1,21 @@
+package com.example.burgermenu.ui.rectsquare;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.burgermenu.R;
+
+public class RectSquareFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_rect_square, container, false);
+
+        return root;
+    }
+}
